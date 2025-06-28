6bcdbd98340fb2e20d16698ff4903147108156eb
belato <sola.belator@gmail.com>
2025-06-28 20:21:32 +0300

    update gendiff.js

--- Makefile
+++ Makefile
@@ -4,5 +4,3 @@ lint:
 		npx eslint
 gendiff:
 		node bin/gendiff.js
-publish:
-		npm publish --dry-run
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,12 @@
 #!/usr/bin/env node
 
-console.log('Hello Artem!')
+import { Command } from 'commander'
+const program = new Command()
+
+program
+  .description('Compares two configuration files and shows a difference.')
+  .argument('<filepath1> <filepath2>')
+  .option('-V, --version', 'output the version number')
+  .option('-f, --format [type]', 'output format')
+
+program.parse()
