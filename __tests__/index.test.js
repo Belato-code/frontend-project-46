ac62a9d41309fb104844a7ab7d17861761748eb7
belato <sola.belator@gmail.com>
2025-07-02 17:36:39 +0300

    add tests

---
+++ __tests__/index.test.js
@@ -0,0 +1,26 @@
+import { fileURLToPath } from 'url'
+import path, { dirname } from 'path'
+import getDifferencies from '../src/index.js'
+import { test, expect } from '@jest/globals'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
+
+test('Find differencies', () => {
+  const strings = [
+    '- follow: false',
+    '  host: hexlet.io',
+    '- proxy: 123.234.53.22',
+    '- timeout: 50',
+    '+ timeout: 20',
+    '+ verbose: true',
+  ]
+
+  const expected = `{\n${strings.map(item => `  ${item}`).join('\n')}\n}`
+  const path1 = getFixturePath('file1.json')
+  const path2 = getFixturePath('file2.json')
+
+  expect(getDifferencies(path1, path2)).toBe(expected)
+})
