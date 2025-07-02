d0054e13c07ca2dc367208a765a337ebaf4529ea
belato <sola.belator@gmail.com>
2025-07-02 20:34:48 +0300

    fix test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,12 +1,6 @@
-import { fileURLToPath } from 'url'
-import path, { dirname } from 'path'
 import getDifferencies from '../src/index.js'
 import { test, expect } from '@jest/globals'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
+import { getFixturePath } from '../src/index.js'
 
 test('Find differencies', () => {
   const strings = [
--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import path from 'node:path'
-import { cwd } from 'node:process'
 import fs from 'node:fs'
 import _ from 'lodash'
+import { cwd } from 'node:process'
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+
+export const getFixturePath = filename => path.join(__dirname, '..', '__fixtures__', filename)
 
 const getDifferencies = (path1, path2) => {
   const file1 = JSON.parse(fs.readFileSync(makePath(path1), 'utf8'))
