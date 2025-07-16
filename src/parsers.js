e88cf69b0f6bff34383a32dd2fca78478ace8129
belato <sola.belator@gmail.com>
2025-07-16 19:43:19 +0300

    update parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,19 +1,14 @@
-import fs from 'fs'
-import path from 'path'
 import yaml from 'js-yaml'
-import { cwd } from 'process'
 
-const makePath = fileName => path.resolve(cwd(), fileName)
 const parsers = {
   '.json': JSON.parse,
   '.yaml': yaml.load,
   '.yml': yaml.load,
 }
 
-export default function makeParse(fileName) {
-  const extention = path.extname(fileName)
+export default function parse(data, extention) {
   if (Object.hasOwn(parsers, extention)) {
-    return parsers[extention](fs.readFileSync(makePath(fileName), 'utf-8'))
+    return parsers[extention](data)
   }
   else {
     throw new Error(`File extention ${extention} is not supported!`)
